f1095120c103124108e7e07936c135e934ac9dfb
jadewisemann <jadewisemann@gmail.com>
2026-01-21 21:17:40 +0900

    use bruteforce

--- 프로그래머스(완전탐색^lv2)모음사전^84512.py
+++ 프로그래머스(완전탐색^lv2)모음사전^84512.py
@@ -1,4 +1,20 @@
 def solution(word):
+    vowels = 'UOIEA'
+    stack = ['']
+    count = -1
+
+    while stack:
+        curr = stack.pop()
+        count += 1
+        
+        if curr == word:
+            return count
+        
+        if len(curr) < 5:
+            for v in vowels:
+                stack.append(curr + v)
+
+def solution_2(word):
     weights = [(31*5+1)*5 + 1, 31*5 + 1, 6*5 + 1, 5*1 + 1, 1]
     vowels = {'A': 0, 'E': 1, 'I': 2, 'O': 3, 'U': 4}
     answer = 0
