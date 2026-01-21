6f388c9035edc65d6f77921c60e97fe543ee116a
jadewisemann <jadewisemann@gmail.com>
2026-01-21 21:52:18 +0900

    two pointer

--- 백준(완전탐색^브론즈2)블랙잭^2798.py
+++ 백준(완전탐색^브론즈2)블랙잭^2798.py
@@ -1,11 +1,19 @@
-from itertools import combinations
-
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
+
+arr.sort()
 ans = 0
 
-for comb in combinations(arr, 3):
-    s = sum(comb)
-    if s <= m:
-        ans = max(ans, s)
+for i in range(n-2):
+    l, r = i+1, n-1
+
+    while l < r:
+        s = arr[i] + arr[l] + arr[r]
+
+        if s <= m:
+            ans = max(ans, s)
+            l += 1
+        else:
+            r -= 1
+
 print(ans)
