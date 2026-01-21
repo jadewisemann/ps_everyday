0eef76af01535c987d0b3570e7d0e11699cedaca
jadewisemann <jadewisemann@gmail.com>
2026-01-21 19:30:38 +0900

    remove some code

--- 프로그래머스(완전탐색^lv2)카펫.py
+++ 프로그래머스(완전탐색^lv2)카펫.py
@@ -11,10 +11,6 @@ def solution(brown, yellow):
             break
         curr += 1
     
-    # 하나면 굳이 안해봐도 유일한 해
-    if len(heights) == 1:
-        return (b_y_sum//heights[0], heights[0])
-
     for w, h in [(b_y_sum//h, h) for h in heights]:
         if yellow == (w - 2) * (h - 2):
             return([w, h])
