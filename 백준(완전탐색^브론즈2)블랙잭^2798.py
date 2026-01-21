ae7b21d3c7fd1d3b8245e6b3fb6be1c9cfaeecdc
jadewisemann <jadewisemann@gmail.com>
2026-01-21 21:43:22 +0900

    itertools, combinations

--- 백준(완전탐색^브론즈2)블랙잭^2798.py
+++ 백준(완전탐색^브론즈2)블랙잭^2798.py
@@ -1,11 +1,11 @@
+from itertools import combinations
+
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 ans = 0
 
-for i in range(n - 2):
-    for j in range(i + 1, n - 1):
-        for k in range(j + 1, n):
-            s = arr[i] + arr[j] + arr[k]
-            if s <= m:
-                ans = max(ans, s)
+for comb in combinations(arr, 3):
+    s = sum(comb)
+    if s <= m:
+        ans = max(ans, s)
 print(ans)
