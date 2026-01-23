f61f6b9e86358d45b8dc0c03907155caedf03c85
Jade Wisemann <139527412+jadewisemann@users.noreply.github.com>
2026-01-23 10:37:51 +0900

    sneak

--- 백준(그래프^실버2)연결_요소의_개수^11724.py
+++ 백준(그래프^실버2)연결_요소의_개수^11724.py
@@ -1,8 +1,31 @@
-ip = lambda:  map(int, input().split())
+import sys
+input = sys.stdin.readline
 
+ip = lambda:  map(int, input().split())
 
 n, m = ip()
 
+roots = list(range(n+1))
+
+def find(x):
+
+    root  = x
+    while roots[root] != root:
+        root = roots[root]
+
+    curr = x
+    while roots[curr] != root:
+        next = roots[curr]
+        roots[curr] = root
+        curr = next
+
+    return root
+
 for _ in range(m):
     u, v = ip()
+
+    ru, rv = find(u), find(v)
+    if ru != rv:
+        roots[rv] = ru
     
+print(sum(1 for i in range(1, n+1) if roots[i] == i))
