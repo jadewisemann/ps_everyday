fd2a78eceb3fc2d693ce3fe5c03d542d3506b3a2
Jade Wisemann <139527412+jadewisemann@users.noreply.github.com>
2026-01-23 09:11:42 +0900

    restore

--- 백준(브루트^브론즈1)ISBN^14626.py
+++ 백준(브루트^브론즈1)ISBN^14626.py
@@ -1,1 +1,12 @@
- sadfksdf
+a, b = list(input().split('*'))
+idx = len(a)
+m = int(b[-1])
+
+for arr in [a + str(i) + b for i in range(10)]:
+    tmp = 0
+    for weight, el in zip([1, 3] * 6, arr[:-1]): 
+        tmp += weight * int(el)
+    
+    if m == (10 - tmp % 10) % 10:
+        print(arr[idx])
+        break
