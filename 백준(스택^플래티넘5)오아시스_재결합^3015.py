19001f227266af63ee904aae50762a4315ad986b
jadewisemann <jadewisemann@gmail.com>
2026-01-26 17:51:01 +0900

    solve

---
+++ 백준(스택^플래티넘5)오아시스_재결합^3015.py
@@ -0,0 +1,30 @@
+# I = lambda: int(input())
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+ans = 0
+stk = []
+
+for curr in arr:
+    
+    while stk and stk[-1][0] < curr:
+        ans += stk.pop()[1]
+    
+    if stk and stk[-1][0] == curr:
+        cnt = stk.pop()[1]
+        ans += cnt
+
+        if stk:
+           ans += 1
+         
+        stk.append((curr, cnt + 1))
+    
+    else:
+        if stk:
+            ans += 1
+        
+        stk.append((curr, 1))
+
+print(ans)
