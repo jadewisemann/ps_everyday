80d0ac0f036dea3ae97a678bff486663f3623961
jadewisemann <jadewisemann@gmail.com>
2026-01-28 17:58:37 +0900

    ..

--- 프로그래머스(스택^lv2)기능개발.py
+++ 프로그래머스(스택^lv2)기능개발.py
@@ -1,6 +1,12 @@
 def solution(progresses, speeds):
     n = len(progresses)
     days = [(100 - progresses[i]) // speeds[i] for i in range(n)]
-    
+    #  7, 3, 9
+    stk = []
+    while stk:
+        
+        curr = stk.pop()
+        
+    # 5, 10, 1, 1, 20 ,1
     answer = []
     return answer
