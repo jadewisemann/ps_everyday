bcf1da64277f4f7dc93d7e20c19e8a379ae54958
jadewisemann <jadewisemann@gmail.com>
2026-01-28 18:55:35 +0900

    solve

---
+++ 프로그래머스(배열^lv2)방문_길이^49994.py
@@ -0,0 +1,21 @@
+DIRS = {
+    "U": ( 0,  1),
+    "D": ( 0, -1),
+    "R": ( 1,  0),
+    "L": (-1,  0),
+}
+
+def solution(dirs):
+    vis = set()
+    sx, sy = 0, 0
+
+    for dir in dirs:
+        dx, dy = DIRS[dir]
+        nx, ny = sx + dx, sy + dy
+        if nx < -5 or nx > 5 or ny < -5 or ny > 5:
+            continue
+        vis.add((sx,sy, nx, ny))
+        vis.add((nx, ny, sx, sy))
+        sx, sy = nx, ny
+        
+    return len(vis) // 2
