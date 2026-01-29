1125689f441cd188aa77ac2ff6b3f1c6e44e6cb6
jadewisemann <jadewisemann@gmail.com>
2026-01-29 15:36:15 +0900

    solve

--- 백준(그리디^골드2)보석_도둑^1202.py
+++ 백준(그리디^골드2)보석_도둑^1202.py
@@ -1,9 +1,33 @@
+import heapq as hq
+
 ip = lambda: map(int, input().split())
 
 n, k = ip()
 
+# 보석
+jewels = [ ]
 for _ in range(n):
-    m, v = ip()
+    jewels.append([*ip()])
 
-for _ in range(m):
-    c = int(input())
+
+bags = []
+for _ in range(k):  
+    bags.append(int(input()))
+
+
+jewels.sort()
+bags.sort()
+
+result = 0
+tmp_jewels = []
+idx = 0
+
+for bag in bags:
+    while idx < n and jewels[idx][0] <= bag:
+        hq.heappush(tmp_jewels, -jewels[idx][1])
+        idx += 1
+
+    if tmp_jewels:
+        result += -hq.heappop(tmp_jewels)
+
+print(result) 
