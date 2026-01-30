a80e695f73c56eaf2bd8ca18cd735db728422e8c
jadewisemann <jadewisemann@gmail.com>
2026-01-30 16:22:47 +0900

    solve

---
+++ 코드트리(그리디^40xp)수_합치기.py
@@ -0,0 +1,16 @@
+import heapq as hq
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+hq.heapify(arr)
+cnt = 0
+
+for _ in range(n-1):
+    a = hq.heappop(arr)
+    b = hq.heappop(arr)
+    cnt += a + b
+    hq.heappush(arr, a + b)
+    
+print(cnt) 
