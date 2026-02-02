0fec4923e735936872b4e3e97232f9970363cd8b
jadewisemann <jadewisemann@gmail.com>
2026-02-02 14:44:26 +0900

    ALTER

--- SWEA(배열^D2)min_max^4828.py
+++ SWEA(배열^D2)min_max^4828.py
@@ -1,5 +1,13 @@
-T = int(input())
-for tc in range(T):
+for tc in range(int(input())):
     n = int(input())
-    arr = list(map(int,input().split()))
-    print(f'#{tc + 1} {max(arr) - min(arr)}') 
+    
+    max_val, min_val = 0, float('inf')
+
+    for el in list(map(int, input().split())):
+        if el > max_val: 
+            max_val = el
+        if el < min_val:
+            min_val = el
+    
+    print(f'#{tc+1} {max_val - min_val}')
+        
