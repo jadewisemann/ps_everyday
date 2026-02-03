bec99e6ddb8ff73cc6f023ab7668d3d80e1acbe6
jadewisemann <jadewisemann@gmail.com>
2026-02-03 09:02:24 +0900

    sync

--- 코드트리(백트래킹^90xp)알파벳과_사칙연산.py
+++ 코드트리(백트래킹^90xp)알파벳과_사칙연산.py
@@ -2,26 +2,35 @@ expression = input()
 
 # Please write your code here.
 ops = []
-char_cnt = 0
+
+
+chars = []
 
 for char in expression:
     if char in "abcdef":
-        char_cnt += 1 
+        chars.append(char)
     else:
         ops.append(char)
 
 max_val = 0
 
-def a(idx, nums):
-    if idx == char_cnt:
-            
-        # 계산
-        # max
+len_char = len(set(chars))
 
-        return 
+def a(idx, nums):
+    if idx == len_char:    
+        # 계산
+        prev = chars[0]
+        for i in range(1,len(chars)):
+            curr_ops = ops[i - 1]
+            curr_nums = chars
+            if curr_ops == '+':
+                prev =
+        # max
+        return
     
     for num in [1, 2, 3, 4]:
         a(idx + 1, [*nums, num])    
+
 
 a(0, [])
 
