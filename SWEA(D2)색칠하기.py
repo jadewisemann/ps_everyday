849571c726b4bae1b44f79a4873743218c323cc4
jadewisemann <jadewisemann@gmail.com>
2026-02-04 16:38:13 +0900

    alter

--- SWEA(D2)색칠하기.py
+++ SWEA(D2)색칠하기.py
@@ -1,18 +1,20 @@
 for tc in range(int(input())):
-    vis = [[0 for _ in range(10 + 1)] for _ in range(10 + 1)]
+    vis = [[[False, False] for _ in range(10 + 1)] for _ in range(10 + 1)]
      
     n = int(input())
-    reds =  set()
-    blues = set()
- 
     for _ in range(n):
         sx, sy, ex, ey, clr = map(int, input().split())
- 
+  
         for nx in range(sx, ex + 1):
             for ny in range(sy, ey + 1):
-                if clr == 1:
-                    reds.add((nx, ny))
-                else:
-                    blues.add((nx, ny))
-       
-    print(f'#{tc + 1} {len(reds & blues)}')
+                vis[nx][ny][clr-1] = True
+  
+    tmp = 0
+    for row in vis:
+        for r, b in row:
+            if r and b:
+                tmp += 1
+      
+    # tmp = sum(r and b for row in vis for r, b in row)
+      
+    print(f'#{tc + 1} {tmp}')
