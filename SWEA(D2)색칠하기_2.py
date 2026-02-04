b29686f97199d22c32236bbb8ca1f63ca99a4147
jadewisemann <jadewisemann@gmail.com>
2026-02-04 19:07:27 +0900

    solve

--- SWEA(D2)색칠하기_2.py
+++ SWEA(D2)색칠하기_2.py
@@ -1,25 +1,23 @@
 for tc in range(int(input())):
     n = int(input())
-    grid = [[0] * 12 for _ in range(12)]
-     
+    grid = [[0] * 10 for _ in range(10)]
+
     for _ in range(n):
         sx, sy, ex, ey, clr = map(int, input().split())
         for nx in range(sx, ex + 1):
             for ny in range(sy, ey + 1):
-                grid[nx][ny] = clr
-    
-    rst = {1: 0, 2: 0}
+                grid[nx][ny] += clr
 
+    ans = 0
     for x in range(10):
         for y in range(10):
             curr = grid[x][y]
-            if not curr:
+            if curr not in [1, 2]:
                 continue
             
-            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 nx, ny = x + dx, y + dy
+                if not (0 <= nx < 10 and 0 <= ny < 10) or grid[nx][ny] != curr:
+                    ans += 1
 
-            if not (0 <= nx < 10 and 0 <= ny < 10) or grid[nx][ny] == 0:
-                rst[curr] += 1
-      
-    print(f"#{tc + 1} {rst[1] + rst[2]}")
+    print(f"#{tc + 1} {ans}")
