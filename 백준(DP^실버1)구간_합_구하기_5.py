6ed4fa71d1e2ffe80b4029eb0de22898495519c4
jadewisemann <jadewisemann@gmail.com>
2026-02-05 16:58:27 +0900

    solve

--- 백준(DP^실버1)구간_합_구하기_5.py
+++ 백준(DP^실버1)구간_합_구하기_5.py
@@ -1,12 +1,18 @@
+import sys
+input = sys.stdin.readline
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
-dp = [[0] * n for _ in range(n)]
+
+dp = [[0] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        dp[i][j] = grid[i - 1][j - 1] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1]
+
 
 for _ in range(m):
     sx, sy, ex, ey = map(int,input().split())
     
-    sums = 0
-    for x in range(sx - 1, ex):
-        for y in range(sy - 1, ey):
-            sums += grid[x][y]
-    print(sums)
+    result = dp[ex][ey] - dp[sx - 1][ey] - dp[ex][sy - 1] + dp[sx - 1][sy -  1]
+
+    print(result)
