5647fa209f8e3093664de52e6db3a02d93d66b24
jadewisemann <jadewisemann@gmail.com>
2026-02-05 17:56:17 +0900

    sync

---
+++ SWEA(D4)Laddder1.py
@@ -0,0 +1,4 @@
+for _ in range(10):
+    tc = int(input())
+    grp = [list(map(int, input().split())) for _ in range(100)]
+    start = 
---
+++ 백준(그래프^골드4)최단경로^1753.py
@@ -0,0 +1,5 @@
+V, E = map(int, input().split())
+N = int(input())
+for _ in range(V):
+    u, v, w = map(int, input().split())
+    
