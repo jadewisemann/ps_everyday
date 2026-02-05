ad66e15f5c51075085df510ebd3a68e23434abd6
Jade Wisemann <139527412+jadewisemann@users.noreply.github.com>
2026-02-06 00:09:22 +0900

    add alter

--- 코드트리(__^90xp)금_채굴하기.py
+++ 코드트리(__^90xp)금_채굴하기.py
@@ -3,23 +3,19 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 max_val = 0
-
 for i in range(n):
     for j in range(n):
-
         for k in range(n + 1):
-            gold_cnt = 0
-
-            for r in range(-k, k + 1):
-                for c in range(-(k - abs(r)), k - abs(r) + 1):
-                    nr, nc = i + r, j + c
-                    if 0 <= nr < n and 0 <= nc < n:
-                        gold_cnt += grid[nr][nc]
+            gold_cnt = sum(
+                grid[i + di][j + dj]
+                for di in range(-k, k + 1)
+                for dj in range(-k, k + 1)
+                if 0 <= i + di < n 
+                and 0 <= j + dj < n 
+                and abs(di) + abs(dj) <= k
+            )
             
-            cost = k ** 2 + (k + 1) ** 2
-            revenue = gold_cnt * m
-
-            if revenue >= cost:
+            if gold_cnt * m >= k ** 2 + (k + 1) ** 2:
                 max_val = max(max_val, gold_cnt)
 
 print(max_val)
