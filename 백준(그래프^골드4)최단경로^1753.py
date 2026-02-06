582e90ea3da69339c8fc0c2fabd13556f2c4da54
jadewisemann <jadewisemann@gmail.com>
2026-02-06 09:41:38 +0900

    solve

--- 백준(그래프^골드4)최단경로^1753.py
+++ 백준(그래프^골드4)최단경로^1753.py
@@ -1,5 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+import heapq
+
+INF = float('inf')
+
 V, E = map(int, input().split())
-N = int(input())
-for _ in range(V):
+K = int(input())
+
+grp = [[] for _ in range(V + 1)]
+dists = [INF] * (V + 1)
+
+for _ in range(E):
     u, v, w = map(int, input().split())
+    grp[u].append((v, w))
+
+q = []
+heapq.heappush(q, (0, K))
+dists[K] = 0
+
+while q:
+    dist, now = heapq.heappop(q)
     
+    if dists[now] < dist:
+        continue
+
+    for neigbor, weight in grp[now]:
+        cost = dist  + weight
+
+        if cost < dists[neigbor]:
+            dists[neigbor] = cost
+            heapq.heappush(q, (cost, neigbor))
+
+
+for i in range(1, V + 1):
+    print(dists[i] if dists[i] != INF else "INF")
