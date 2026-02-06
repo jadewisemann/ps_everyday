d1a78cc45eecde5ae5db4b286397376fc61765ae
jadewisemann <jadewisemann@gmail.com>
2026-02-06 10:31:27 +0900

    add solution using set

--- 백준(그래프^실버1)케빈_베이컨의_6단계_법칙^using_set.py
+++ 백준(그래프^실버1)케빈_베이컨의_6단계_법칙^using_set.py
@@ -1,31 +1,41 @@
-from collections import deque as dq 
-
 n,m = map(int, input().split())
-
-friends = [[] for _ in range(n + 1)]
+adj = [set() for _ in range(n + 1)]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    friends[a].append(b)
-    friends[b].append(a)
+    adj[a].add(b)
+    adj[b].add(a)
 
-kevin_numbers = []
+min_kevin_number = None
+winner = -1
 
 for i in range(1, n + 1):
-    dists = [-1] * (n + 1)
-    dists[i] = 0
-    que = dq([i])
+    dist = [0] * (n + 1)
+    vis = {i}
+    curr = {i}
+    step = 1
 
-    while que:
-        curr = que.popleft()
-        for friend in friends[curr]:
-            if dists[friend]  == -1:
-                dists[friend] = dists[curr] + 1
-                que.append(friend)
+    while curr:
+        next = set()
+        for node in curr:
+            for neighbor in adj[node]:
+                if neighbor not in vis:
+                    vis.add(neighbor)
+                    next.add(neighbor)
+                    dist[neighbor] = step
+        curr = next
+        step += 1
     
-    kevin_numbers.append(sum(dist for dist in dists if dist > 0))
+    kevin_number = sum(dist)
+    if (
+        min_kevin_number is None 
+        or kevin_number < min_kevin_number
+    ):
+        min_kevin_number = kevin_number
+        winner = i
 
-print(kevin_numbers.index(min(kevin_numbers)) + 1)
+print(winner)
+
 
 
             
