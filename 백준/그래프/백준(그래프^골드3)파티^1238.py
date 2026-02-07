dabd5f282695b703bda661b0dbf215c81285b9ba
jadewisemann <jadewisemann@gmail.com>
2026-02-07 20:57:12 +0900

    solve(baekjoon): #1238

--- 백준/그래프/백준(그래프^골드3)파티^1238.py
+++ 백준/그래프/백준(그래프^골드3)파티^1238.py
@@ -1,45 +1,39 @@
 import heapq
 
-INF = float('inf')
-
 def dijkstra(start, n, graph):
     dists = [float('inf')] * (n + 1)
     dists[start] = 0
-    hq = [(0, start)]
+    min_heap = [(0, start)]
+    while min_heap:
+        dist, curr = heapq.heappop(min_heap)
+        
+        if dist > dists[curr]:
+            continue
 
-    while hq:
-        dist, curr = heapq.heappop(hq)
+        for neighbor, weight in graph[curr]:
+            dist_to_neighbor = dist + weight
 
+            if dist_to_neighbor >= dists[neighbor]:
+                continue
+
+            dists[neighbor] = dist_to_neighbor
+            heapq.heappush(min_heap, (dist_to_neighbor, neighbor))
+    return [dist if dist != float('inf') else 0 for dist in dists]
 
 n, m, x = map(int, input().split())
 
-grp = [[] for _ in range(n + 1)]
+adj_std = [[] for _ in range(n + 1)]
+adj_rev = [[] for _ in range(n + 1)]
+
 for _ in range(m):
     s, e, w = map(int, input().split())
-    grp[s].append((e, w))
-    grp[e].append((s, w))
-
-dists = [[INF] for _ in range(n + 1)]
-
-hq = []
-heapq.heappush(hq, (0, x))
-dists[x] = 0
-
-while hq:
-    dist, curr = heapq.heappop()
-
-    if dists[curr] < dist:
-        continue
-
-    for neigbor, weight in grp[curr]:
-        cost = dist + weight
-
-        if cost < dists[neigbor]:
-            dists[neigbor] = cost
-            heapq.heappush(hq, (cost, neigbor))
+    adj_std[s].append((e, w))
+    adj_rev[e].append((s, w))
 
 
-    
+dists_to_x = dijkstra(x, n, adj_rev)
+dists_from_x = dijkstra(x, n, adj_std)
 
 
+print(max(map(lambda a, b: a + b, dists_from_x, dists_to_x)))
 
