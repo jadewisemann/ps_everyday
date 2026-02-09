208e721545baaeb77863ce5c104f00fb30f9e1b2
jadewisemann <jadewisemann@gmail.com>
2026-02-09 14:06:34 +0900

    solve(baekjoon): #1918.py

--- 백준/스택/백준(스택^골드2)후위_표기시^1918.py
+++ 백준/스택/백준(스택^골드2)후위_표기시^1918.py
@@ -1,15 +1,24 @@
 
 stk = []
 res = ""
+prec = {'*': 3, '/': 3, '+': 2, '-': 2, '(': 1}
+
 for char in input():
     if char.isalpha():
        res += char
     elif char == "(":
         stk.append(char)
     elif char == ")":
-        while stk[-1] == "(":
+        while stk and stk[-1] != "(":
             res += stk.pop()
+        stk.pop()
     else:
-        while stk[-1] >= 
-        # ? 
+        while stk and prec[stk[-1]] >= prec[char]:
+            res += stk.pop()
+        stk.append(char)
+
+while stk:
+    res += stk.pop()
+
+print(res)
         
