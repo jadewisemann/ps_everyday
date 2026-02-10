2f2cf215632e32bf07c92b81ccca6d7fd25fb092
jadewisemann <jadewisemann@gmail.com>
2026-02-10 19:10:05 +0900

    solve(SWEA): 풍선팡 보너스 게임2

---
+++ SWEA/SWEA(D2)풍선팡_보너스게임2.py
@@ -0,0 +1,14 @@
+for tc in range(int(input())):
+    n = int(input())
+    grid = [list(map(int, input().split())) for _ in range(n)]
+    print(f'#{tc + 1}', 
+        max(
+            sum(
+                grid[i][k] + grid[k][j]
+                for k in range(n)
+            ) - grid[i][j]
+            for i in range(n)
+            for j in range(n)
+        )
+    )
+
