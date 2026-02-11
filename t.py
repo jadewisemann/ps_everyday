5d3950c425358a4b57dd11c1721aa2e791a1ee0d
jadewisemann <jadewisemann@gmail.com>
2026-02-11 19:43:03 +0900

    template(tree): disjoint set, union & find

--- t.py
+++ t.py
@@ -1,5 +1,22 @@
-n = 4
-m = 4
-visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
-for row in visited:
-    print(row)
+for _ in range(4):
+    x1, y1, p1, q1, x2, y2, p2, q2 = map(int, input().split())
+    x_cnt, y_cnt = 0, 0
+    for x in range(x1, p1+1):
+        if x2 <= x <= p2:
+            x_cnt += 1
+        if x_cnt > 1:
+            break
+    for y in range(y1, q1+1):
+        if y2 <= y <= q2:
+            y_cnt += 1
+        if y_cnt > 1:
+    if not x_cnt and not y_cnt:
+        code = 'd'
+    elif x_cnt == 1 and y_cnt == 1:
+        code = 'c'
+    elif x_cnt == 1 or y_cnt == 1:
+        code = 'b'
+    else:
+        code = 'a'
+
+    print(code)
---
+++ template/tree(disjoint_set)union_find.py
@@ -0,0 +1,25 @@
+def find(parent, x):
+    if parent[x] == x: return x
+    parent[x] = find(parent[x])
+    return parent[x]
+
+def union(parent, x, y):
+    root_x, root_y = find(x), find(y)
+    if root_x != root_y:
+        parent[root_y] = root_x
+
+def union(parent, rank, x, y):
+    root_x, root_y = find(parent, x), find(parent, y)
+
+    if root_x == root_y:
+        return False
+    
+    if rank[root_x] < rank[root_y]:
+        parent[root_x] = root_y
+    elif rank[root_x] > rank[root_y]:
+        parent[root_y] = root_x
+    else:
+        parent[root_y] = root_x
+        rank[root_x] += 1
+    return True
+    
