5e23c384196365714df28048bbd21b2b8ab140cb
jadewisemann <jadewisemann@gmail.com>
2026-02-11 21:46:32 +0900

    solve(codetree): 2차원 바람

---
+++ 백준/구현/백준(구현^골드1)2048_easy^12100.py
@@ -0,0 +1,18 @@
+# 5번?
+dirs = ((-1, 0), (1, 0), (0, -1), (0, 1))
+
+#TODO permutation 만들기
+
+#TODO 블록 이동 함수 만들기
+
+#TODO 병함 함수 만들기
+
+#TODO dir 처리 함수 만들기
+b
+
+
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+
+
+.....................,
