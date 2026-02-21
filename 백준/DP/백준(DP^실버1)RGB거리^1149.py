0ab87a1f82c1168ce4a5e76b8b5bcf4ebb4e488a
jadewisemann <jadewisemann@gmail.com>
2026-02-21 18:31:06 +0900

    modify(problem): 잘못입력된 코드 수정

--- 백준/DP/백준(DP^실버1)RGB거리^1149.py
+++ 백준/DP/백준(DP^실버1)RGB거리^1149.py
@@ -1,16 +1,8 @@
-for tc in range(int(input())):
-    counts = [0] * 10
-    n = int(input())
- 
-    for card in input():
-        card = int(card)
-        counts[card] += 1
- 
-    max_card, max_count = 0, 0
-    for curr_card in range(1, 10):
-        curr_count = counts[curr_card]
-        if curr_count >= max_count:
-            max_count = curr_count
-            max_card = curr_card 
-      
-    print(f'#{tc + 1} {max_card} {max_count}')
+N = int(input())
+grp = [list(map(int, input().split())) for _ in range(N)]
+max_arr = [*grp[0]]
+max_temp = [*grp[0]]
+for i in range(1,N):
+    max_temp = [grp[i][0]+min(max_arr[1],max_arr[2]), grp[i][1] + min(max_arr[0], max_arr[2]),grp[i][2]+min(max_arr[0], max_arr[1]),]
+    max_arr = max_temp; max_temp = [0,0,0]
+print(min(max_arr))
