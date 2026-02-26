82187defa2356c9b88eeafe4dfcd54249a9d4aa8
Jade Wisemann <139527412+jadewisemann@users.noreply.github.com>
2026-02-26 17:56:55 +0900

    solve(BOJ)

--- 백준/(문제집)삼성_SW_역량_테스트_기출_문제/BOJ(__)[Gold4]테트로미노#14500.py
+++ 백준/(문제집)삼성_SW_역량_테스트_기출_문제/BOJ(__)[Gold4]테트로미노#14500.py
@@ -1,3 +1,41 @@
-# BOJ(__)[Gold4]테트로미노#14500.py
-# 삼성 SW 역량 테스트 기출 문제
+def solution():
+    n, m = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    drc = ((0, 1), (0, -1), (1, 0), (-1, 0))
 
+    max_val_possible = max(max(row) for row in arr)
+    
+    def dfs(i, r, c, w, global_max):
+        if w + (4 - i) * max_val_possible <= global_max:
+            return global_max
+
+        if i == 4:
+            return max(global_max, w)
+
+        for dr, dc in drc:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < n and 0 <= nc < m and arr[nr][nc]:
+                tmp = arr[nr][nc]
+                
+                arr[nr][nc] = 0
+                
+                if i == 2:
+                    global_max = dfs(i + 1, r, c, w + tmp, global_max)
+                
+                global_max = dfs(i + 1, nr, nc, w + tmp, global_max)
+                
+                arr[nr][nc] = tmp
+                
+        return global_max
+
+    ans = 0
+    for i in range(n):
+        for j in range(m):
+            v = arr[i][j]
+            arr[i][j] = 0
+            ans = dfs(1, i, j, v, ans)
+            arr[i][j] = v
+            
+    print(ans)
+
+solution()
--- 백준/(문제집)삼성_SW_역량_테스트_기출_문제/BOJ(__)[Silver2]퇴사#14501.py
+++ 백준/(문제집)삼성_SW_역량_테스트_기출_문제/BOJ(__)[Silver2]퇴사#14501.py
@@ -1,3 +1,25 @@
-# BOJ(__)[Silver2]퇴사#14501.py
-# 삼성 SW 역량 테스트 기출 문제
+n = int(input())
+tps = [tuple(map(int, input().split())) for _ in range(n)]
 
+def recur(i):
+    return (
+        0 if i >= n else 
+        max(tps[i][1] + recur(i + tps[i][0]) if i + tps[i][0] <= n else 0, recur(i + 1))
+    )
+
+print(recur(0)) 
+
+
+n = int(input())
+tps = [list(map(int, input().split())) for _ in range(n)]
+dp = [0] * (n + 1)
+
+for i in range(n - 1, -1, -1):
+    time, price = tps[i]
+    
+    if i + time <= n:
+        dp[i] = max(price + dp[i + time], dp[i + 1])
+    else:
+        dp[i] = dp[i + 1]
+
+print(dp[0])
