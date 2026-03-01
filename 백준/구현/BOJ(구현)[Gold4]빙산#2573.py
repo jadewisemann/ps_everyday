0177ac9910ec376cd52f1d02fe0dcbdcd01109f8
jadewisemann <jadewisemann@gmail.com>
2026-03-01 20:22:55 +0900

    optimization

--- 백준/구현/BOJ(구현)[Gold4]빙산#2573.py
+++ 백준/구현/BOJ(구현)[Gold4]빙산#2573.py
@@ -1,63 +1,129 @@
-import sys
-input = sys.stdin.readline
 
-n, m = map(int, input().split())
 
-grid = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
+def solve(n, m, grid, iceberg_count, si, sj):
+    
+    max_cells = n * m
 
-icebergs = {
-    (i, j)
-    for i in range(n)
-    for j in range(m)
-    if grid[i][j]    
-}
+    queue_i = [0] * max_cells
+    queue_j = [0] * max_cells
+    melt_i = [0] * max_cells
+    melt_j = [0] * max_cells
+    melt_amount = [0] * max_cells
 
-cnt = 0
-while True:
-    if not icebergs:
-        cnt = 0
-        break
-  
-    start_iceberg = next(iter(icebergs))
-    stack = [start_iceberg]
-    vis = {start_iceberg}
 
-    while stack:
-        ci, cj = stack.pop()
-        for di, dj in ((1, 0), (0, 1), (-1, 0), (0, -1)):
-            ni, nj = ci + di, cj + dj
+    visited = [[0] * m for _ in range(n)]
+    
+    year = 0
+
+    while iceberg_count > 0:
+        year += 1
+
+        queue_head = 0
+        queue_tail = 0
+
+        queue_i[queue_tail] = si
+        queue_j[queue_tail] = sj
+        queue_tail += 1
+        visited[si][sj] = year
+
+        melt_tail = 0
+
+        while queue_head < queue_tail:
+            ci = queue_i[queue_head]
+            cj = queue_j[queue_head]
+            queue_head += 1
+
+            adj_sea_count = 0
             
-            if (ni, nj) not in icebergs: continue
-            if (ni, nj) in vis: continue
+            ni = ci - 1
+            if grid[ni][cj] == 0:
+                adj_sea_count += 1
+            elif visited[ni][cj] != year:
+                visited[ni][cj] = year
+                queue_i[queue_tail] = ni
+                queue_j[queue_tail] = cj
+                queue_tail += 1
+                
+            ni = ci + 1
+            if grid[ni][cj] == 0:
+                adj_sea_count += 1
+            elif visited[ni][cj] != year:
+                visited[ni][cj] = year
+                queue_i[queue_tail] = ni
+                queue_j[queue_tail] = cj
+                queue_tail += 1
+                
+            nj = cj - 1
+            if grid[ci][nj] == 0:
+                adj_sea_count += 1
+            elif visited[ci][nj] != year:
+                visited[ci][nj] = year
+                queue_i[queue_tail] = ci
+                queue_j[queue_tail] = nj
+                queue_tail += 1
+                
+            nj = cj + 1
+            if grid[ci][nj] == 0:
+                adj_sea_count += 1
+            elif visited[ci][nj] != year:
+                visited[ci][nj] = year
+                queue_i[queue_tail] = ci
+                queue_j[queue_tail] = nj
+                queue_tail += 1
+            
+            if adj_sea_count > 0:
+                melt_i[melt_tail] = ci
+                melt_j[melt_tail] = cj
+                melt_amount[melt_tail] = adj_sea_count
+                melt_tail += 1
+            
+        if queue_tail != iceberg_count:
+            return year - 1
+        
+        for k in range(melt_tail):
+            mi, mj = melt_i[k], melt_j[k]
 
-            vis.add((ni, nj))
-            stack.append((ni, nj))
+            grid[mi][mj] -= melt_amount[k]
+
+            if grid[mi][mj] <= 0:
+                grid[mi][mj] = 0
+                iceberg_count -= 1
+        
+        if iceberg_count > 0:
+            for k in range(queue_tail - 1, -1, -1):
+                if grid[queue_i[k]][queue_j[k]] > 0:
+                    si, sj = queue_i[k], queue_j[k]
+                    break
     
-    if len(vis) != len(icebergs):
-        break 
+    return 0
 
-    melting_amount = []
-    for ci, cj in icebergs:
-        sea_count = 0
-        for di, dj in ((1, 0), (0, 1), (-1, 0), (0, -1)):
-            ni, nj = ci + di, cj + dj
-            if not (0 <= ni < n and 0 <= nj < m): continue
-            if grid[ni][nj]: continue
-            sea_count += 1
-        if sea_count > 0:
-            melting_amount.append((ci, cj, sea_count))
+def main():
 
-    for ci, cj, amount in melting_amount:
-        if grid[ci][cj] <= amount:
-            grid[ci][cj] = 0
-            icebergs.remove((ci, cj))
-        else: 
-            grid[ci][cj] -= amount
+    input_data = open(0).read().split()
+
+    n = int(input_data[0])
+    m = int(input_data[1])
+    
+    raw_data = list(map(int, input_data[2:]))
+
+    grid = [
+        raw_data[i * m : (i + 1) * m]
+        for i in range(n)
+    ]
+
+    iceberg_count = 0
+    si, sj = 0, 0
+
+    for i in range(1, n - 1):
+        for j in range(1, m - 1):
+            if grid[i][j] > 0:
+                iceberg_count += 1
+                si, sj = i, j
+
+    ans = solve(n, m, grid, iceberg_count, si, sj)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
     
     
-    cnt += 1
-
-print(cnt)
